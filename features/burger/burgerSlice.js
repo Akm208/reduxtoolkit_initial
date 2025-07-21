2eb02313cf717931d8cdda7d2b6b0067f048abe7
Akm208 <akmalmuhammad554@gmail.com>
2025-07-21 16:49:09 +0500

    asyncThunk

--- features/burger/burgerSlice.js
+++ features/burger/burgerSlice.js
@@ -1,3 +1,5 @@
+const { pizzaActions } = require('../pizza/pizzaSlice');
+
 const createSlice=require('@reduxjs/toolkit').createSlice;
 const initialState={
  burgerBuns:200
@@ -10,6 +12,11 @@ const burgerSlice=createSlice({
         burger_order:(state)=>{
             state.burgerBuns--
         }
+    },
+    extraReducers:(builder)=>{
+        builder.addCase(pizzaActions.pizza_order,(state)=>{
+            state.burgerBuns--
+        })
     }
 })
 console.log(burgerSlice);
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const store =require('./app/store');
+const { fetchProducts } = require('./features/products/productSlice');
 const pizzaActions=require('./features/pizza/pizzaSlice').pizzaActions
 const burgerActions=require('./features/burger/burgerSlice').burgerActions
 console.log("initialState",store.getState());
@@ -7,8 +8,12 @@ const unsubscribe=store.subscribe(()=>{
     console.log("Update the state", store.getState());
     
 })
-store.dispatch(pizzaActions.pizza_order())
-store.dispatch(pizzaActions.pizza_order())
-store.dispatch(burgerActions.burger_order())
-store.dispatch(burgerActions.burger_order())
+store.dispatch(fetchProducts()).then(()=>{
+    console.log("Update the state", store.getState());
+    
+})
+// store.dispatch(pizzaActions.pizza_order())
+// store.dispatch(pizzaActions.pizza_order())
+// store.dispatch(burgerActions.burger_order())
+// store.dispatch(burgerActions.burger_order())
 unsubscribe()
